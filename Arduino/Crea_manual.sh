5dd66fb3848beb4aa2d51e1bfd8f0434edaece77
javacasm <javacasm@gmail.com>
2017-04-21 12:59:46 +0200

    formato

--- Arduino/Crea_manual.sh
+++ Arduino/Crea_manual.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#pandoc --latex-engine=xelatex    -V papersize:a4paper    --template=./LaTeX_ES.latex    -o "Tema 1 - Introducción a Arduino.pdf"     Cabecera.md        "Tema 1 - Introducción a Arduino.md"
-# pandoc --latex-engine=xelatex   -V papersize:a4paper    --template=./LaTeX_ES.latex    -o "Tema 2 - Hardware de  Arduino.pdf"     Cabecera.md        "Tema 2 - Hardware de  Arduino.md"
+pandoc --latex-engine=xelatex   -V papersize:a4paper    --template=./LaTeX_ES.latex    -o "Tema 1 - Introducción a Arduino.pdf"     Cabecera.md        "Tema 1 - Introducción a Arduino.md"
+pandoc --latex-engine=xelatex   -V papersize:a4paper    --template=./LaTeX_ES.latex    -o "Tema 2 - Hardware de  Arduino.pdf"     Cabecera.md        "Tema 2 - Hardware de  Arduino.md"
 
 pandoc --latex-engine=xelatex \
+      --from=markdown \
       -V papersize:a4paper \
       --template=./LaTeX_ES.latex \
       -o "Tema 3 - Programación de  Arduino.pdf" \
@@ -11,4 +12,4 @@ pandoc --latex-engine=xelatex \
       "Tema 3 - Programación de  Arduino.md"
 
 
-# pandoc --latex-engine=xelatex        -V papersize:a4paper       --template=./LaTeX_ES.latex       -o "Tema 4 - Controlando el mundo físico con Arduino.pdf"       Cabecera.md           "Tema 4 - Controlando el mundo físico con Arduino.md"
+pandoc --latex-engine=xelatex        -V papersize:a4paper       --template=./LaTeX_ES.latex       -o "Tema 4 - Controlando el mundo físico con Arduino.pdf"       Cabecera.md           "Tema 4 - Controlando el mundo físico con Arduino.md"
